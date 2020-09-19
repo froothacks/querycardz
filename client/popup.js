64910cef55cc66062cbe1c9cd21ba82f26c44941
allen <allenlu@live.ca>
2020-09-19 01:55:00 -0400

    redirect button press to newpage.html

--- client/popup.js
+++ client/popup.js
@@ -12,10 +12,14 @@ chrome.storage.sync.get('color', function(data) {
 });
 
 changeColor.onclick = function(element) {
-  let color = element.target.value;
+  /*let color = element.target.value;
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
         {code: 'document.body.style.backgroundColor = "' + color + '";'});
   });
+  */
+ chrome.tabs.create({'url': chrome.extension.getURL('newpage.html')}, function(tab) {
+  // Tab opened.
+ });
 };
