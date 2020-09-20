5fb0e1db662701c60704f77e255ff6f850ec0e97
Advait-M <advait.maybhate9@gmail.com>
2020-09-20 05:27:10 -0400

    fix spellcheck

--- server/main.py
+++ server/main.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 @app.route('/', methods=["POST"])
 def storeQueryForUser():
     search_query = request.args.get('query')
-    search_query = spellchecker.spell(search_query)
+    search_query = spellchecker(search_query)
     email = request.args.get('email')
     print(email)
 
