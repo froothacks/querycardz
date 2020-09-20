e8b7ec368898081ed24da4dbb1f629458acf7c37
Advait-M <advait.maybhate9@gmail.com>
2020-09-19 20:31:41 -0400

    HOOK UP ALL THE THINGS

--- server/main.py
+++ server/main.py
@@ -1,19 +1,37 @@
-from flask import Flask
-from flask_pyoidc.flask_pyoidc import OIDCAuthentication
+from flask import Flask, request
+# from flask_pyoidc.flask_pyoidc import OIDCAuthentication
 import textinput
-import atexit
+import topics
+import database
+import questions
+import json
+# import atexit
 
+app = Flask(__name__)
 
 @app.route('/', methods=["POST"])
 def storeQueryForUser():
     search_query = request.args.get('query')
-    textinput.getAnswer(search_query)
+    if isQ.predict_question(search_query):
+        answer = textinput.getAnswer(search_query)
+        if answer is not None:
+            query_topics = topics.getTopics(search_query, answer)
+            print("sending", search_query, answer, query_topics)
+            db.search_query("test@test.ca", search_query, answer, query_topics)
+        else:
+            print("no assistant response:", search_query)
+    else:
+        print("not a question:", search_query)
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
 
 
-@atexit.register
-def shutdown():
-    client.disconnect()
+# @atexit.register
+# def shutdown():
+#     client.disconnect()
 
 
 if __name__ == '__main__':
+    isQ = questions.IsQuestion()
+    db = database.QueryCardzDatabase("querycardz", "auth/ibm_credentials.json")
+    db.connect()
     app.run()
