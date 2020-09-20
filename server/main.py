5a4c597414be60c78cf4b12a50b600d5c7fdfbf2
Advait-M <advait.maybhate9@gmail.com>
2020-09-20 05:19:20 -0400

    spellcheck (might be broken)

--- server/main.py
+++ server/main.py
@@ -4,6 +4,7 @@ import textinput
 import topics
 import database
 import questions
+import spellcheck
 import json
 # import atexit
 
@@ -13,6 +14,7 @@ app = Flask(__name__)
 @app.route('/', methods=["POST"])
 def storeQueryForUser():
     search_query = request.args.get('query')
+    search_query = spellcheck.spell(search_query)
     email = request.args.get('email')
 
     print("sq", search_query, request.args)
@@ -39,6 +41,7 @@ def getCardsForUser():
 
 if __name__ == '__main__':
     isQ = questions.IsQuestion()
+    spell = spellcheck.Speller()
     db = database.QueryCardzDatabase("querycardz", "auth/ibm_credentials.json")
     db.connect()
     app.run()
